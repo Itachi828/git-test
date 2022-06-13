b7a763b31b3d78518313aa8c2416766dca6c6cbb
Itachi <745623438@qq.com>
2022-06-13 10:58:31 +0800

    Ideal first commit!

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Itachi
+ * @Date: 2022/6/13 10:54
+ * @Version: jdk1.8
+ * @Description: Ideal测试git
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
