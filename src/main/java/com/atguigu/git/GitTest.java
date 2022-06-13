099a4db15c5588d309336ccd869c000063ff46eb
Itachi <745623438@qq.com>
2022-06-13 11:03:50 +0800

    Ideal third commit!

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,7 +8,8 @@ package com.atguigu.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("git test");
-        System.out.println("git test");
+        System.out.println("git test1");
+        System.out.println("git test2");
+        System.out.println("git test3");
     }
 }
